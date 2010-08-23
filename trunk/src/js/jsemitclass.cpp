80a7984646fc75b3cc381129ccc8420070b73336
torben.weis <torben.weis@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2010-08-23 21:00:37 +0000

    Implemented the emit(key,value) function in JavaScript for executing JavaScript-based mapping on the digest

--- trunk/src/js/jsemitclass.cpp
+++ trunk/src/js/jsemitclass.cpp
@@ -21,6 +21,11 @@ QVariant JSEmitClass::extension( Extension extension, const QVariant& argument )
             context->throwError("emit() accepts exactly two parameters");
             return QVariant();
         }
+        if ( !context->argument(0).isArray() )
+        {
+            context->throwError("The first argument to emit() must be an array");
+            return QVariant();
+        }
         qDebug("emit(%s,%s)", qPrintable(context->argument(0).toString()), qPrintable(context->argument(1).toString()));
         return QVariant(true);
     }
