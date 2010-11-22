cb601565388c65190633c8c3215e086e8da36aa6
torben.weis <torben.weis@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2010-11-22 00:40:28 +0000

    Implemented wave federation HTTP handler. Still some wiring inside lightwave TODO to hook wave-OT up with the rest of lightwave

--- gosrc/server/main.go
+++ gosrc/server/main.go
@@ -6,6 +6,8 @@ import (
   "log"
   "fmt"
   "strings"
+  "os"
+  "wave"
 )
 
 var servers map[string]*lightwave.Server = make(map[string]*lightwave.Server)
@@ -16,23 +18,81 @@ func errorHandler(writer http.ResponseWriter, req *http.Request, errorText strin
   fmt.Fprintf(writer, "<p>%s</p>", errorText)
 }
 
-// Maps wave federation requests to requests of the generalized federation protocol.
-// Therefore, this handler is just performing some URL rewriting
-func waveFederationHandler(w http.ResponseWriter, r *http.Request) {
-  // TODO
-}
-
-func clientHandler(w http.ResponseWriter, r *http.Request) {
-  // Determine the virtual host
-  host := r.Host
-  if index := strings.Index(r.Host, ":"); index != -1 {
+// Determine the virtual host
+func findServer(host string) (server *lightwave.Server, err os.Error) {
+  // Strip off the port number
+  if index := strings.Index(host, ":"); index != -1 {
 	host = host[:index]
   }
   server, ok := servers[host]
   if !ok {
-	errorHandler(w, r, "Unknown virtual server: " + host)
+	return nil, os.NewError("Unknown virtual server: " + host)
+  }
+  return server, nil
+}
+
+// Maps wave federation requests to requests of the generalized federation protocol.
+// Therefore, this handler is performing some URL rewriting
+func waveFederationHandler(w http.ResponseWriter, r *http.Request) {
+  // Determine the virtual host
+  server, err := findServer(r.Host)
+  if err != nil {
+	errorHandler(w, r, err.String())
 	return
-  }	
+  }
+  // The URL is of the form http://host/wave/fed/wave-host/wave-id/wavelet-host/wavelet-id    
+  waveurl, err := wave.NewWaveUrlFromId(r.URL.Path[5:])
+  if err != nil {
+	errorHandler(w, r, "Error parsing wave ID")
+	return
+  }
+  uri, _ := lightwave.NewURI("/" + waveurl.WaveletDomain + "/" + waveurl.WaveId + "$" + waveurl.WaveDomain + "$" + waveurl.WaveletId)
+  // GET handler
+  if r.Method == "GET" {
+	ch := make(chan bool)
+	req := &lightwave.GetRequest{lightwave.Request{w, r.URL.RawQuery, ch, lightwave.FederationOrigin, uri}}
+	server.Get( req )
+	<- ch
+	log.Println("GET finished")
+  // POST handler
+  } else if r.Method == "POST" || r.Method == "PUT" {
+	// TODO: Meaningful limit on the size of data
+	if r.ContentLength < 0 || r.ContentLength > 100000 {
+	  errorHandler(w, r, "Negative or oversize content length in HTTP POST body")
+	  return
+	}
+	buffer := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(buffer)
+	if err != nil {
+	  errorHandler(w, r, "Error reading from HTTP POST body")
+	  return
+	}
+	ch := make(chan bool)
+	req := &lightwave.PostRequest{lightwave.Request{w, r.URL.RawQuery, ch, lightwave.FederationOrigin, uri}, buffer, ""}
+	var ok bool
+	if req.MimeType, ok = r.Header["Content-Type"]; !ok {
+	  errorHandler(w, r, "Content-Type is missing in POST")
+	  return
+	}
+	if req.MimeType != "application/x-protobuf-wave" {
+	  errorHandler(w, r, "Content-Type " + req.MimeType + " is not understood")
+	  return
+	}
+	server.Post( req )
+	<- ch
+	log.Println("POST finished")
+  } else {
+	errorHandler(w, r, "Unsupported HTTP method")
+  }  
+}
+
+func clientHandler(w http.ResponseWriter, r *http.Request) {
+  // Determine the virtual host
+  server, err := findServer(r.Host)
+  if err != nil {
+	errorHandler(w, r, err.String())
+	return
+  }
   // Parse the URI, strip off the "/client" in front
   uri, ok := lightwave.NewURI(r.URL.Path[7:])
   if !ok {
@@ -74,16 +134,12 @@ func clientHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func federationHandler(w http.ResponseWriter, r *http.Request) {
-  // Determine the virtual server
-  host := r.Host
-  if index := strings.Index(r.Host, ":"); index != -1 {
-	host = host[:index]
-  }
-  server, ok := servers[host]
-  if !ok {
-	errorHandler(w, r, "Unknown virtual server: " + host)
+  // Determine the virtual host
+  server, err := findServer(r.Host)
+  if err != nil {
+	errorHandler(w, r, err.String())
 	return
-  }	
+  }
   // Parse the URI, strip off the "/fed" in front
   uri, ok := lightwave.NewURI(r.URL.Path[4:])
   if !ok {
