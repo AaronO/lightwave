73665dd5a4434672669cdc813beba162e32afcfc
torben.weis@gmail.com <torben.weis@gmail.com@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2011-01-26 02:35:51 +0000

    Fix

--- trunk/gosrc/server/main.go
+++ trunk/gosrc/server/main.go
@@ -333,6 +333,8 @@ func main() {
   server.AddChild(f)
   // TODO: This is a hack
   server.LocalHost().Users().CreateUser("weis")
+  server.LocalHost().Users().CreateUser("tux")
+  server.LocalHost().Users().CreateUser("konqi")
   // TODO: This is a hack
   if _, err := server.UserAccountDatabase.FindUser("weis"); err != nil {
     server.UserAccountDatabase.SignUpUser("weis@mail.com", "Torben", "weis", "pass")
